9d835dd4a02a6b553e31ef4e8e1e9d5d3a3fd9e7
amalie246 <amni002@egms.no>
2025-09-19 15:52:56 +0200

    init commit

---
+++ src/main/java/com/amalie/demo/DevopsLecture5Application.java
@@ -0,0 +1,13 @@
+package com.amalie.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsLecture5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsLecture5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalie/demo/DevopsLecture5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalie.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsLecture5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
